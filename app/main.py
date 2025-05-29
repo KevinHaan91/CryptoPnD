6dc04e9802437c9eaa70f1c9d2dd174aca26fd64
Kevin Haan <khaan1@wgu.edu>
2025-05-29 07:39:10 -0700

    fuck this

--- app/main.py
+++ app/main.py
@@ -57,4 +57,4 @@ async def get_pump_score(symbol: str = "BTCUSDT"):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
